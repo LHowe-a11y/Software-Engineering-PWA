620be8c2a6feb0670705c726cc57459ec6f636c0
LH <liam.howe2@education.nsw.gov.au>
2024-11-08 16:36:34 +1100

    Downloaded CSV data, created script to format it, added to table

---
+++ myPWA/.database/csvreader.py
@@ -0,0 +1,13 @@
+import csv
+i = -1
+with open("myPWA/.database/Newdata.txt", 'w') as w:
+    with open("myPWA/.database/australian_public_holidays_2025.csv", newline='') as f:
+        for row in csv.reader(f):
+            i += 1
+            if i == 0:
+                continue
+            w.write('(')
+            for x in row:
+                w.write('"'+x+'",')
+            w.write(str(i))
+            w.write('),'+'\n')
